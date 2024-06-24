bb864f8457b3cf9b1aacfaf805508936566101dc
jtuakalau <johntuakalau@gmail.com>
2024-06-25 00:19:15 +1000

    update gitignote

--- EKS/main.tf
+++ EKS/main.tf
@@ -30,7 +30,7 @@ module "vpc" {
 #EKS
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
-  cluster_name                   = "my-eks-cluster"
+  cluster_name                   = "jtlab-cluster"
   cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
