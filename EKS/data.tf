664fea2051057d648e1adeabbd997b65afecc848
jtuakalau <johntuakalau@gmail.com>
2024-06-27 00:18:10 +1000

    update git

--- EKS/data.tf
+++ EKS/data.tf
@@ -2,9 +2,10 @@ data "aws_availability_zones" "azs" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
--- EKS/main.tf
+++ EKS/main.tf
@@ -33,7 +33,7 @@ module "vpc" {
 
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
-  cluster_name                   = "my-eks-cluster"
+  cluster_name                   = "jtlab-cluster"
   cluster_version                = "1.30"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
