03740343f9933dcd71bf3ef3f40010074ef7b75b
jtuakalau <johntuakalau@gmail.com>
2024-06-26 23:04:42 +1000

    update id

--- EKS/data.tf
+++ EKS/data.tf
@@ -1,11 +1,10 @@
 data "aws_availability_zones" "azs" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
-  depends_on = [ module.eks
-]
+  depends_on = [module.eks]
 }
